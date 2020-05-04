a7328ebf6d34e4f357221a873c82b868190e0e98
TemmyScope <esdentp@gmail.com>
2020-05-04 19:13:24 +0100

    Router 3.0.0 with improved speed and injector capability

--- Route.php
+++ Route.php
@@ -12,32 +12,47 @@ use \Closure;
 
 class Route
 {
-	private $routes = [];
-	private $prefix = "";
-	private $middleware = [];
-	private $Process_middleware = [];
+	const NOT_FOUND = 1;
+	const METHOD_NOT_ALLOWED = 2;
 
-	public function __construct($namespace = '', $cache_dir = __DIR__)
+	private $routes = [];
+	private $cached = false;
+	private $fallback = [];
+	private $prefix = "";
+	private $dir  = "";
+	private $inject = [ ]; // ['group_name' => [$var1, $var2]//array of objects or variable injected ]
+	private $middleware = [];
+	private $Process_middleware = ['default' => ""];
+
+	public function __construct($namespace = '')
 	{
-		$this->url= ( isset($_SERVER['PATH_INFO'])) ? rtrim($_SERVER['PATH_INFO'] , '/') : '/';
+		//$this->url= ( isset($_SERVER['PATH_INFO'])) ? rtrim( , '/') : '/';
 		$this->namespace= $namespace;
-		$this->request_method = strtolower($_SERVER['REQUEST_METHOD']);
-		$this->dir = $cache_dir;
-		$this->cached = $this->getCache();
+		$this->fallback[1] = function(){ echo "404: Resource Not Found."; };
+		$this->fallback[2] = function(){ echo "405: METHOD NOT ALLOWED."; };
 	}
 
-	public function group(array $array, Callable $fn)
+	public function enableCache($cache_dir = __DIR__)
 	{
-		if (is_array($array['middleware']))
-		$array['middleware'][0] = $this->namespace.'\\'.$array['middleware'][0];
-		
+		$this->dir = $cache_dir;
+		$this->cached = @include $this->dir.'/route7.cache.php' ?? false;
+	}
+
+	public static function init($namespace = '')
+	{
+		return new self($namespace);
+	}
+	
+	public function group(array $array, \Closure $fn)
+	{	
 		if ($this->cached === false) {
 			@$this->middleware['name'] = $array['name'];
 			@$this->middleware['_middle_'] = $array['middleware'];
 			$this->prefix = $array['prefix'] ?? "";
-			$fn($this);	
+			$fn($this);
 		}
 		@$this->Process_middleware[$array['name']] = $array['middleware'];
+		@$this->inject[$array['name']] = $array['inject'];
 	}
 
 	public function __call($method, $args)
@@ -45,93 +60,91 @@ class Route
 		if( $this->cached === false ){
 			$method = strtolower($method);
 			$uri = $this->prefix.strtolower($args[0]);
-			$callable = $args[1];
-			if( is_array($callable) ){
-				$callable[0] = $this->namespace.'\\'.$callable[0];
-			}elseif(is_string($callable)){
-				$callable = serialize($callable);
-			}else{
-				$callable = serialize(new SerializableClosure($callable));
-			}
+			$callable = $this->processCallable($args[1]);
 			if (!empty($this->middleware['name'])) {
 				$this->routes[$this->middleware['name']][$method][$uri] =  $callable;
 			} else {
 				$this->routes['default'][$method][$uri] =  $callable;
-			}	
+			}
 		}
 	}
 
-	public function run(){
-		$routes = [];
-		if ($this->cached) {	
-			$routes = $this->cached;
-			unset($this->cached);
-		} else {
-			$this->setCache();
-			$routes = $this->routes;
-			unset($this->routes);
-		}
+	public function setFallback(\Closure $fallback, $case)
+	{
+		$this->fallback[$case] = $fallback;
+	}
+
+	public function run(string $method, string $url){
+		$routes = $this->getRoutes();
+		$call = $this->routeChecker($routes, $method, $url);
 		
-		if(@$fn = $routes['default'][$this->request_method][$this->url]){
-			return $this->diLoad($fn);
-		}else{
-			$exp = explode('/', $this->url);
-			$param = $this->sanitize(array_pop($exp));
-			$url_to_uri = implode('/', $exp).'/';
-			if (@$fn = $routes['default'][$this->request_method][$url_to_uri] ) {
-				return $this->diLoad($fn, [$param]);
+		if ( isset($call['middleware']) ) {
+			if ( $call['middleware'] != "" ) {
+				$route = new Route;
+				return $this->diLoad( $call['middleware'], [ function() use ($route, $call){
+					return $route->diLoad( $call['call'], $call['param'] );
+				}]);
 			}
+			return $this->diLoad($call['call'], $call['param'] );
 		}
-
-		$call = [];
-		foreach ($this->Process_middleware as $key => $value) {
-			if ( @$fn = $routes[$key][$this->request_method][$this->url] ) {
-				$call ['call'] = $fn;
-				$call['param'] = [];
-				$call['middleware'] = $value;
-				break;
-			}
-			$exp = explode('/', $this->url);
-			$param = $this->sanitize(array_pop($exp));
-			$url_to_uri = implode('/', $exp).'/';
-			if (@$fn = $routes[$key][$this->request_method][$url_to_uri] ) {
-				$call ['call'] = $fn;
-				$call['param'] = [$param];
-				$call['middleware'] = $value;
-				break;
-			}
-		}
-		if(!empty($call)){
-			$route = new Route;
-			$this->diLoad($call['middleware'], [ function() use ($route, $call){
-				return $route->diLoad($call['call'], $call['param']);
-			}]);
-		}else{
-			return http_response_code(404);
-		}
+		if ( !isset($routes[$method]) ) {
+			return $this->diLoad( $this->fallback[2] );
+		} return $this->diLoad( $this->fallback[1] );
 	}
 
-	protected function getCache()
-	{
-		return @include $this->dir.'/route7.cache.php' ?? false;
-	}
-
-	protected function setCache()
-	{
-		file_put_contents($this->dir.'/route7.cache.php', "<?php return ".var_export($this->routes, true).";", LOCK_EX);
-	}
-
-	protected function diLoad($fn, $params = []){
-		$callable = is_string($fn) ? unserialize($fn) : $fn;
+	protected function diLoad( $fn, $params = [] ){
 		$builder = new DI\ContainerBuilder();
 		$builder->enableCompilation($this->dir . '/tmp');
 		$builder->writeProxiesToFile(true, $this->dir . '/tmp/proxies');
-		$builder->useAnnotations(false);
 		$container = $builder->build();
-		$container->call($callable, $params);
+		$container->call( is_string($fn) ? unserialize($fn) : $fn, $params);
 	}
 
-	private function sanitize($dirty){
-        return htmlentities($dirty, ENT_QUOTES, 'UTF-8');;
-  	}
+	protected function routeChecker(array $routes, string $method, string $url): array
+	{
+		$method = strtolower($method);
+		$call = [];
+		foreach ($this->Process_middleware as $key => $value) {
+			if(@$fn = $routes[$key][$method][$url]){
+				$call ['call'] = $fn;
+				@$call['param'] = $this->inject[$key] ?? [];
+				$call['middleware'] = $value;
+				return $call;
+			}
+		}
+		$exp = explode('/', $url);
+		$param = htmlentities(array_pop($exp), ENT_QUOTES, 'UTF-8');
+		$url_to_uri = implode('/', $exp).'/';
+		foreach ($this->Process_middleware as $key => $value) {
+			if (@$fn = $routes[$key][$method][$url_to_uri] ) {
+				$call ['call'] = $fn;
+				@$call['param'] = $this->inject[$key] ?? [];
+				array_push( $call['param'], $param );
+				$call['middleware'] = $value;
+				return $call;
+			}
+		}
+		return $call;
+	}
+
+	protected function processCallable($callable)
+	{
+		if( is_array($callable) ){
+			$callable[0] = $this->namespace.'\\'.$callable[0];
+			return $callable;
+		}elseif(is_string($callable)){
+			return serialize($callable);
+		} return serialize(new SerializableClosure($callable));
+	}
+
+	protected function getRoutes(): Array
+    {
+        if ($this->cached === false) {
+			if ( $this->dir != "") {
+				file_put_contents($this->dir.'/route7.cache.php', "<?php return ".var_export($this->routes, true).";", LOCK_EX);
+			}
+            return $this->routes;
+		}
+        return $this->cached;
+	}
 }
--- readme.md
+++ readme.md
@@ -4,55 +4,12 @@
 
 SevenRouter is an extremely lightweight and simple fast router
 
-The Router class should not be used as a router for API. It processes user-enetered url into an available 
-previously defined controller & endpoint.
-
-E.g.
- url => /user/edit
-
- is Processed into	=> UserController::class, 'edit' method
-
-The usage of Router Class of this library looks something like this:
-
-```php
-session_start();
-use Seven\Router\Router;
-
-require __DIR__.'/vendor/autoload.php';
-
-$router = new Router([
-	'namespace' => 'App\Controllers',
-	//default Controller & method as fallback
-	'controller' => AuthController::class,
-	'method' => "index",
-	'cache_dir' => __DIR__.'/cache'
-]);
-if (($_SESSION['id'])) { 
-	//To restricts certain routes from being accessed without a certain session set
-	$router->call([
-		'SearchController' => [],
-		'HomeController' => [ 'index' ],
-		'AccountController' => ['balance', 'index'],
- 		'ProfileController' => [ 'edit', 'index']
-	]);
-} else {
-	$router->call([
-		'AuthController' => [ 'index', 'login', 'register', 'forgot_password', 'activate', 'about', 'logout'],
-		'ErrorsController' => ['_404', '_405', 'bad', 'denied', 'unknown']
-	]);
-}
-```
-
-
-Note: The difference between a route that expects a parameter and one that doesn't is the trailing slash in the route. E.g.
-
-/user/ =>represents a route thta expects a parameter/variable in the request url, such as /user/1
-/user => represents the /user route and expects no parameteror variable
+The Router Class of this Library has been discontinued.
 
 In order to make the routes recompile and show newly added route(s), delete the already compiled route7.cache.php file from the directory you provided to the Route library, as well as all the files in the /tmp sub-directory.
 
 
-The usage of the Route Class of this library looks something like this:
+The usage of the Router looks something like this:
 ```php
 use Seven\Router\Route;
 
@@ -60,7 +17,19 @@ require __DIR__.'/vendor/autoload.php';
 //This accepts the namespace for the controllers that would be used. and the cache directory for the compiled routes
 //both parameters are required
 
-$route = new Route('App\Controllers', __DIR__.'/cache');
+$route = new Route('App\Controllers'); //Route::init('App\Controllers', __DIR__.'/cache');
+
+$router->enableCache(__DIR__.'/cache'); //comment this line on a development server
+
+//If the two fallbacks are not set, the router handles both automatically
+$router->setFallback(function(){
+	return print_r(['error' => 404]);
+}, Route::NOT_FOUND);
+
+$router->setFallback(function(){
+	return print_r(['error' => 405]);
+}, Route::METHOD_NOT_ALLOWED);
+
 
 $route->get('/', function(){
 	echo 'The api is ready';
@@ -69,7 +38,6 @@ $route->get('/', function(){
 function show(){
 	echo 'The version is 1';
 }
-
 $route->get('/version', 'show');
 
 $route->get('/login', [ AuthController::class, "login" ]);
@@ -78,20 +46,14 @@ $route->get('/login', [ AuthController::class, "login" ]);
 $route->post('/register', [ AuthController::class, "register" ] );
 $route->get('/home',  [ HomeController::class, 'index' ]);
 
-$route->group(['prefix' => '/api'], function($route){
-
-	$route->get('/search', [ SearchController::class, 'index' ]);
-	$route->get('/search/', [ SearchController::class, 'index' ]);
-	$route->post('/user', [ UserController::class, 'index' ]);
-	$route->get('/users', [ UserController::class, 'index' ]);
-	$route->put('/user/', [ UserController::class, 'update' ]);
-
-});
-$route->group(['prefix' => '/restricted', 
+//note that when giving route groups name, 'default' is a reserved name in the library, so don't use it.
+$route->group(['prefix' => '/api/restricted', 
 				'name' => 'auth',
-				'middleware' => [ AuthController::class, "index"] //the midleware should expect a closure $next param
+				'inject' => [$req, $res],
+				//the midleware should be fully namespaced middleware and must expect a closure $next param
+				'middleware' => [ App\Controllers\AuthController::class, "index"]
 		], function($route){
-	$route->get('/search', [ UserController::class, 'index' ]);
+	$route->get('/user', [ UserController::class, 'index' ]);
 	$route->get('/search/', [ UserController::class, 'index' ]);
 	$route->post('/user', [ UserController::class, 'index' ]);
 	$route->get('/users', [ UserController::class, 'index' ]);
@@ -99,5 +61,11 @@ $route->group(['prefix' => '/restricted',
 	$route->post('/user/add', [ UserController::class, 'add' ]);
 });
 
-$route->run(); //this is where the router actually decides which response to be returned
+//this is where the router actually decides which response to be returned
+$router->run($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
 ```
+
+Note: The difference between a route that expects a parameter and one that doesn't is the trailing slash in the route. E.g.
+
+/user/ =>represents a route thta expects a parameter/variable in the request url, such as /user/1
+/user => represents the /user route and expects no parameteror variable
