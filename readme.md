174d0647a98f50fdc4750416bac4d45fab0c526a
temmyscope <esdentp@gmail.com>
2020-10-03 20:45:01 -0700

    Heavy updates on new Router.php, yet to be tested

--- readme.md
+++ readme.md
@@ -1,15 +1,35 @@
 ## About SevenRouter
 
-=> SevenRouter is developed by Elisha Temiloluwa [ TemmyScope ]	
+=> Seven Router is developed by Elisha Temiloluwa [ TemmyScope ]	
 
-SevenRouter is an extremely lightweight and simple fast router
-
-The Router Class of this Library has been discontinued.
-
-In order to make the routes recompile and show newly added route(s), delete the already compiled route7.cache.php file from the directory you provided to the Route library, as well as all the files in the /tmp sub-directory.
+=> The Library uses the PHP-DI dependency container to inject dependencies.
 
 
-The usage of the Router looks something like this:
+# Installation
+```bash
+composer require sevens/router
+```
+
+# PHP >=7.4 Performance Hack
+	
+	=> Use preloader to preload the cached route file after compilation on a production server.
+
+## Seven\Router\Route
+
+	=> The Route Class is an extremely lightweight and simple fast router
+
+In order to make the routes recompile and show newly added route(s), 
+delete the already compiled route7.cache.php file from the directory 
+you provided to the Route library, as well as all the files in the /tmp sub-directory.
+
+Note: The difference between a route that expects a parameter and one 
+that doesn't is the trailing slash in the route. 
+E.g.:
+/user/ =>represents a route thta expects a parameter/variable in the request url, such as /user/1
+/user => represents the /user route and expects no parameteror variable
+
+# The usage of the Router\Route looks something like this:
+
 ```php
 use Seven\Router\Route;
 
@@ -18,14 +38,14 @@ require __DIR__.'/vendor/autoload.php';
 
 $route = new Route('App\Controllers'); //Route::init('App\Controllers');
 
-$router->enableCache(__DIR__.'/cache'); //comment this line on a development server
+$route->enableCache(__DIR__.'/cache'); //comment this line on a development server
 
 //If the two fallbacks are not set, the router handles both automatically
-$router->setFallback(function(){
+$route->setFallback(function(){
 	return print_r(['error' => 404]);
 }, Route::NOT_FOUND);
 
-$router->setFallback(function(){
+$route->setFallback(function(){
 	return print_r(['error' => 405]);
 }, Route::METHOD_NOT_ALLOWED);
 
@@ -44,6 +64,7 @@ $route->post('/login', [ AuthController::class, "login" ]);
 $route->get('/login', [ AuthController::class, "login" ]);
 $route->post('/register', [ AuthController::class, "register" ] );
 $route->get('/home',  [ HomeController::class, 'index' ]);
+
 
 //note that when giving route groups name, 'default' is a reserved name in the library, so don't use it.
 $route->group(['prefix' => '/api/restricted', 
@@ -66,7 +87,168 @@ $router->run($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
 //$router->run($_SERVER['REQUEST_METHOD'], $_SERVER['PATH_INFO'] ?? '/');
 ```
 
-Note: The difference between a route that expects a parameter and one that doesn't is the trailing slash in the route. E.g.
+## Seven\Router\Router
 
-/user/ =>represents a route thta expects a parameter/variable in the request url, such as /user/1
-/user => represents the /user route and expects no parameteror variable
+# From Version 3.1.0, The more conventional Router Library has been added to the Seven\Router Library.
+Although quite faster than most Router libraries, the Router Class handles request slower than the
+Route Class which does not accept parameters.
+
+# The usage of the Router\Router is explained below.
+
+
+# Initialize the class
+```php
+	use \Seven\Router\Router;
+
+	#namespace: refers to the namespace from which the classes should be loaded
+	$route = new Router($namespace = 'App\Controllers');
+```
+
+# Performance Optimization and Cache
+	
+	=> To Improve performance on a production server, you should enable cache.
+
+	=> Downside: Whenever a new route is added, You have to delete the cache file from 
+	the cache directory in order for the new route to be reloaded.
+
+```php
+	$route->enableCache($directory = __DIR__.'/cache');
+```
+
+
+# Register PSR-7 Compliant/Implementing Request & Response Objects
+	
+	=> registering request & or response object that violates PSR-7 Interface would result in errors.
+```php
+	/**
+	* @param ServerRequestInterface $request
+	* @param ResponseInterface $response
+	* 
+	* @return void
+	*/
+
+	$route->registerProviders($request, $response);
+```
+
+
+# Register middlewares you want to use later in your route calls:: All Callables are acceptable
+
+```php
+	#register middlewares E.g. for authentication, cors etc. using callables expecting the request, response, next
+	$route->middleware('cors', 
+		function(ServerRequestInterface $request, ResponseInterface $response, $next){
+		#do something with request or set headers for response
+		$headers = [
+      'Access-Control-Allow-Origin'      => '*',
+      'Access-Control-Allow-Methods'     => '*',
+      'Access-Control-Allow-Credentials' => 'true',
+      'Access-Control-Max-Age'           => '86400',
+      'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
+	  ];
+	  if ($request->isMethod('OPTIONS')){
+	      return $response->json('{"method":"OPTIONS"}', 200, $headers);
+	  }
+	  foreach($headers as $key => $value){
+	      $response->header($key, $value);
+	  }
+		#if required conditions are met do:
+		$next($request, $response);
+	});
+```
+
+# The "next" is an object of the PSR-15 RequestHandlerInterface
+
+	=> This means the handle method is available as well as it can be invoked like a function.
+
+```php
+	$route->middleware('auth', 
+		function(ServerRequestInterface $request, ResponseInterface $response, RequestHandlerInterface $next){
+		#do something with request or set headers for response
+		
+		#if required conditions are met do:
+		$next->handle($request);
+	});
+```
+
+# It is best when routes are defined in a different file
+	=> route definition are included/required/loaded into the current file
+
+	=> Note: routes can also be defined in the front controller;
+
+```php
+	require __DIR__.'/routes.php'; 
+
+	//OR
+
+	$route->load(__DIR__.'/routes.php');
+```
+
+# The routing process starts here
+
+	=> The "run" method processes routes and calls the appropriate action if the request succeeds.
+
+```php
+	$route->run();
+
+```
+
+
+# The route difinition in the route file that was required in the front controller e.g. index.php
+
+	=> All Standard Http Methods are supported: GET, POST, PUT, OPTIONS, PATCH, HEAD, DELETE;
+	
+	=>All methods accept a callable as the second parameter 
+
+	=>All callables are injected with the request & response objects that were previously registered
+
+```php
+	$route->get('/post/:id/creator/:name', function($request, $response){
+
+	});
+```
+
+# To make all requests to a certain endpoint return the same callable, use the "all" method
+```php
+	$route->all('/posts', function($request, $response){
+		return $response->json("This handles all requests to /posts endpoint, regardless of request method");
+	});
+```
+
+# All params in uri are accessible through the request param object
+```php
+	$route->put('/post/:key', function($request, $response){
+			return $response->json("This is a request containing key: ". $request->param->key )
+	});
+```
+
+# The "use" method is used to call registered middlewares before returning the endpoint's callable
+	=> The middlewares are called in the order in which they were written/passed
+
+	=> The second parameter passed to the "use" method must be a closure that accepts no parameter
+
+```php
+	#cors middleware is called first in this case.
+	$route->use(['middleware' => ['cors', 'auth'],'prefix'=>'api' ], function() use ($route){}
+		//shorthand: $route->use('cors,auth;prefix:api;', function() use ($route){
+		$route->get('/post/:id', function($request, $response){
+
+		});
+		
+		# request & response objects are passed as arguments automagically
+		$route->post('/post', [ PostController::class, 'create' ]);
+
+	});
+```
+
+# There is a shorthand way to use the "use" method (Of-course it is negligibly slower, if you're performance-conscious)
+```php
+	$route->use('cors,auth;prefix:api;', function() use ($route){
+		$route->get('/post/:id', function($request, $response){
+
+		});
+		
+		# request & response objects are passed as arguments automagically
+		$route->post('/post', [ PostController::class, 'create' ]);
+
+	});
+```
---
+++ src/Router.php
@@ -0,0 +1,398 @@
+<?php
+
+namespace Seven\Router;
+
+/**
+* @author Elisha Temiloluwa a.k.a TemmyScope (temmyscope@protonmail.com)
+* @package Seven Router Package
+*/
+
+use \DI;
+use \Closure;
+use \SplFileObject;
+use Seven\Vars\Strings;
+use Opis\Closure\SerializableClosure;
+use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Http\Message\{ ServerRequestInterface, ResponseInterface };
+
+class Router implements RequestHandlerInterface
+{
+    /**
+    * @property [] app
+    * all the middlewares used in the app router
+    */
+    protected $app = [];
+
+    /**
+    * @property [] | bool cache
+    * contains array of routes loaded from cache, if not available fallbacks to cache
+    */
+    protected $cache = false;
+
+    /**
+    * @property callable
+    */
+    protected $callableAction;
+
+    /**
+    *
+    * @property null | SplFileObject $file
+    */
+    protected $file = null;
+
+    /**
+    * @property [] $middleware
+    */
+    private $middleware = [];
+
+    /**
+    * @property [] params
+    * refers to the key value pairs of expected parameter name as key and value passed as associated value
+    */
+    protected $params = [];
+
+    /**
+    *  @property string prefix
+    * route prefix
+    */
+    protected $prefix = "";
+
+    /**
+    * @property [] routes
+    * all the processed routes
+    */
+    protected $routes = [];
+
+    /**
+    * @property callable[]
+    * array of middlewares allocated to the current route in the order in which they were allocated.
+    */
+    protected $routeMiddlewares = [];
+
+    /**
+    * @property ServerRequestInterface $request
+    * PSR-7 request object
+    */
+    protected $request;
+
+    /**
+    * @property ResponseInterface $request
+    * PSR-7 response object
+    */
+    protected $response;
+
+
+    public function __construct(string $namespace)
+    {
+        $this->namespace = $namespace;
+        $this->next = Route::class;
+    }
+
+    /**
+    * @method __call
+    * responds to GET, POST, PUT, DELETE, OPTIONS, HEAD, PATCH
+    *
+    * @return void
+    */
+    public function __call($method, $args)
+    {
+        if ($this->cache === false) {
+        	if( is_string($args[0]) ){
+        		[ $route, $routeArray, $action ] = $this->unpackRequest($args);
+            	[ $routeParams, $parametized ] = $this->parseRoute($routeArray);
+        	}else{
+        		foreach ($args[0] as $value) {
+        			$this->$method($value, $args[1]);
+        		}
+        		return;
+        	}
+
+            $method = strtolower($method);
+            if (!$parametized) {
+                $this->routes['u'][$method][$route] = [
+                    'callable' => $this->serializeCallable($action), 'middlewares' => $this->middleware
+                ];
+            } else {
+                $size = count($routeArray);
+                $startPosition = (empty($routeArray[0])) ? '/' : $routeArray[0];
+                $this->routes['p'][$method][$size][$startPosition] = [
+                    'callable' => $this->serializeCallable($action), 'middlewares' => $this->middleware,
+                    'params' => $routeParams,  'route' => $routeArray
+                ];
+            }
+        }
+    }
+
+    public function __invoke($request)
+    {
+        if (empty($this->routeMiddlewares)) {
+            return $this->diLoad(
+                $this->prepareCallable($this->callable),
+                [ $this->addParams($request, $this->params), $this->response ]
+            );
+        } else {
+            $middleware = array_shift($this->routeMiddlewares);
+            return $this->diLoad(
+                $middleware,
+                [ $this->request, $this->response, $this->next ]
+            );
+        }
+    }
+
+    private function addParams($request, array $key_value): RequestInterface
+    {
+        $request->params = new \stdClass();
+        foreach ($key_value as $key => $value) {
+                $request->params->$key = $value;
+        }
+        return $request;
+    }
+
+    /**
+    * @method call
+    * @param callable $callable
+    * @param [] $params
+    *
+    * @return mixed
+    */
+    protected function call(callable $callable, $params = [])
+    {
+        $builder = new DI\ContainerBuilder();
+        $builder->enableCompilation(__DIR__ . '/../../../cache/tmp');
+        $builder->writeProxiesToFile(true, __DIR__ . '/../../../cache/tmp/proxies');
+        $container = $builder->build();
+        $container->call($callable, $params);
+    }
+
+    /**
+    * @method enableCache
+    *
+    * @param string $directory
+    *
+    * @return void
+    */
+    public function enableCache($directory): void
+    {
+        $this->cache = @include $directory . '/cache/iroute7.cache.php' ?? false;
+        if ($this->cache === false) {
+            $this->file = new SplFileObject($directory . '/cache/iroute7.cache.php');
+        }
+    }
+
+    protected function findRouteMatch(string $uri, array $relatedRoutes): array
+    {
+        [ $uriArray, $size ] = $this->preProcessUri($uri);
+        if (@$similarRoutes = $relatedRoutes[$size][$uriArray[0]]) {
+            foreach ($similarRoutes as $key => $value) {
+                if ($this->matchUriPatterns($value['params'], $uriArray, $value['route']) === true) {
+                    return $value;
+                }
+            }
+        }
+        return [];
+    }
+
+    /**
+    * @method handle
+    * @param  ServerRequestInterface $request
+    * @return ResponseInterface
+    * implemetation of PSR-15 RequestHandlerInterface
+    */
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        return $this->next($request);
+    }
+
+    public function load(string $routeDirectory)
+    {
+        require $routeDirectory;
+    }
+    
+    /**
+    * @method matchUriPatterns
+    *
+    * @param [] $paramKeyValuePairs
+    * @param [] $uriArray
+    * @param [] $routeArray
+    *
+    * @return bool
+    */
+    protected function matchUriPatterns(array $paramKeyValuePairs, array $uriArray, array $routeArray): bool
+    {
+        $params = [];
+        foreach ($paramKeyValuePairs as $position => $value) {
+            $params[$value] = $uriArray[(int)$position];
+            $uriArray[$position] = $value;
+        }
+        if ($uriArray === $routeArray) {
+            $this->setParams($params);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+    * @method middleware
+    *
+    * @param string $name
+    * @param callable $middleare
+    *
+    * @return void
+    */
+    public function middleware(string $name, callable $middleware)
+    {
+        $this->app[$name] = $middleware;
+    }
+
+    /**
+    * @method parseRoute
+    *
+    * @param [] $routeArray
+    *
+    * @return array
+    */
+    protected function parseRoute(array $routeArray): array
+    {
+        $parametized = false;
+        $routeParams = [];
+        foreach ($routeArray as $position => $value) {
+            if (str_contains($value, ':')) {
+                $routeParams[$position] = str_replace(':', '', $value);
+                $parametized = true;
+            }
+        }
+        return [$parametized, $routeParams];
+    }
+
+    /**
+    * @method prepareCallable
+    * @param string $entry
+    *
+    * @return callable
+    */
+    public function prepareCallable(string $entry)
+    {
+        return unserialize($entry);
+    }
+
+    /**
+    * @method preProcessUri
+    *
+    * @param string $uri
+    *
+    * @return array routeCollection
+    */
+    protected function preProcessUri(string $uri): array
+    {
+        $uriArray = explode('/', $uri);
+        return [$uriArray, count($uriArray)];
+    }
+
+    /**
+    * @method process
+    *
+    * @param string $method
+    * @param string $uri
+    * @param array routeCollection
+    *
+    */
+    protected function process(string $method, string $uri, array $routeCollection)
+    {
+        if (@$route = $routesCollection['u'][$method][$uri] || 
+        	@$route = $routesCollection['u']['all'][$uri] ) {
+            $this->setRouteCallable($route['callable']);
+            
+            $this->setRouteMiddlewares($route['middlewares']);
+        } else {
+            if (@$relatedRoutes = $routeCollection['p'][$method]) {
+                $match = $this->findRouteMatch($uri, $relatedRoutes);
+                if (empty($match)) {
+                    return $this->response->withStatus(404, "Resource does not exist.");
+                }
+                $this->setRouteCallable($match['callable']);
+                $this->setRouteMiddlewares($match['middlewares']);
+            } else {
+                return $this->response->withStatus(500, "Http Method does not exist.");
+            }
+        }
+        return $this->next($this->request, $this->response);
+    }
+
+    public function registerProviders(ServerRequestInterface $request, ResponseInterface $response)
+    {
+        $this->request = $request;
+        $this->response = $response;
+    }
+
+    public function retrieveCache($cache)
+    {
+        if ($this->file !== null) {
+            $this->file->fwrite("<?php return " . var_export($cache, true));
+        }
+        return $cache;
+    }
+
+    protected function routesCollection(): array
+    {
+        return ($this->cache === false) ? $this->routes : $this->retrieveCache($this->cache);
+    }
+
+    public function run()
+    {
+        $this->process(
+            strtolower($this->request->getMethod()),
+            strtolower($this->request->getUri()),
+            $this->routesCollection()
+        );
+    }
+
+    public function serializeCallable(callable $callable): string
+    {
+        if ($callable instanceof \Closure) {
+            $callable = new SerializableClosure($callable);
+        }
+        if (is_array($callable)) {
+            $callable = [ $this->namespace . '\\' . $callable[0], $callable[1] ];
+        }
+        return serialize($callable);
+    }
+
+    protected function setParams($params): void
+    {
+        $this->params = $params;
+    }
+
+    protected function setRouteMiddlewares(array $routeMiddlewares = []): void
+    {
+        $middlewares = [];
+        if (!empty($routeMiddlewares)) {
+            foreach ($routeMiddlewares as $key => $value) {
+                $middlewares[] = $this->app[$value];
+            }
+        }
+        $this->routeMiddlewares = $middlewares;
+    }
+
+    protected function setRouteCallable($routeCallable): void
+    {
+        $this->callable = $routeCallable;
+    }
+
+    protected function unpackRequest($args): array
+    {
+        return [strtolower($args[0]), explode('/', $args[0]), $args[1] ];
+    }
+
+    public function use($middles, \Closure $next)
+    {
+        if (is_array($middles)) {
+            $this->prefix = $middles['prefix'];
+            $this->middleware = $middles['middleware'];
+        } else {
+            $break = explode(';', $middles);
+            $this->middleware = explode(',', $break[0]);
+            $this->prefix = str_replace('prefix:', $break[1]);
+        }
+        $next();
+    }
+}
